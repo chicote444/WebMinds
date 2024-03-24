62e93d6b5884270dd5e30575e3e4f80ba4c9ea12
chicote444 <chicohaikal@gmail.com>
2024-03-24 00:07:21 -0300

    botão de minimizar

--- programa_estudos/index.js
+++ programa_estudos/index.js
@@ -1,1 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+var chico = (a, b) => {
+    return a + b
+};
+
+
+
+function min() {
+    let w = document.querySelector('.progs2');
+    let x = document.querySelector('.progs3');
+    let y = document.querySelector('.turma3');
+    let z = document.querySelector('.turma2');
+    let li = document.querySelector('.link');
+    let prog = document.querySelector('.progs');
+    x.innerHTML = '';
+    y.innerHTML = '';
+    w.innerHTML = '';
+    z.innerHTML = '';
+    prog.style.padding = '0 0 0 0';
+    li.innerHTML = '<a href="index.html">Máximizar</a>';
+    console.log(3);
+}
+
