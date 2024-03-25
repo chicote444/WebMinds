3695368fee5c85d3b19440b199bf7bfa4a09769d
chicote444 <chicohaikal@gmail.com>
2024-03-25 15:40:58 -0300

    JSON integrado no programa de estudos

--- programa_estudos/index.js
+++ programa_estudos/index.js
@@ -20,3 +20,40 @@ var min = () => {
     console.log(3);
 }
 
+fetch('../index.json')
+.then(response => response.json())
+.then((json) => {
+
+    newjson = JSON.stringify(json);
+    newjson = JSON.parse(newjson);
+    console.log(newjson.semana1[0].assuntos[0]);
+    let y = document.querySelectorAll('td');
+    let x = -1
+    console.log(y.length/3);
+   // y[0].innerHTML = newjson.semana1[0].assuntos[0];
+    console.log(newjson.semana1[0].assuntos.length);
+    console.log(newjson.length);
+    for (c = 0; c < 3; c++) {
+
+        for (i = 0; i < newjson.semana1.length; i++) {
+            for (j = 0; j < newjson.semana1[i].assuntos.length; j++) {
+                
+                switch (c) {
+
+                    case 1:
+                        y[x += 1].innerHTML = newjson.semana2[i].assuntos[j];
+                        break;
+
+                    case 2:
+                        y[x += 1].innerHTML = newjson.semana3[i].assuntos[j];
+                        break;
+
+                    case 0:
+                        y[x += 1].innerHTML = newjson.semana1[i].assuntos[j];
+                }
+                console.log(newjson.semana1[c].assuntos.length);
+            }
+            
+        }
+    }
+})
